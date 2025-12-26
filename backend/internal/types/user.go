58e73774ed00dc41818778ae15ec4a9cfacefa2a
RaphaelYeo <Raphael_YEO@htx.gov.sg>
2025-12-27 00:26:01 +0800

    add model methods for users and topics

--- backend/internal/types/user.go
+++ backend/internal/types/user.go
@@ -4,6 +4,11 @@ type CreateUserRequest struct {
 	Username string `json:"username"`
 }
 
+type UpdateUserRequest struct {
+	Username string `json:"username"`
+	IsActive bool   `json:"is_active"`
+}
+
 type UserResponse struct {
 	ID        string    `json:"id"`
 	Username  string    `json:"username"`
