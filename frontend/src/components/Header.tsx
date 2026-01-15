ff531bfc9b7db7b2be72ea80f05b40c3d6403b20
RaphaelYeo <Raphael_YEO@htx.gov.sg>
2026-01-15 15:14:02 +0800

    style NavBar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,28 +1,36 @@
 import { Link } from "react-router-dom";
+import type { JSX } from "react/jsx-dev-runtime";
 
 type HeaderProps = {
   variant: "main" | "sub";
-  title: string | undefined;
+  title: React.ReactNode;
 };
 
 export default function Header({ variant, title }: HeaderProps) {
   if (variant === "main") {
-    return (
-        <h1 className="text-2xl font-bold text-white">{title}</h1>
-    );
+    return <h1 className="text-4xl font-bold">{title}</h1>;
   }
 
-  return (
-    <header className="bg-gray-200 text-gray-800 p-2">
-      <h2 className="text-xl font-semibold">{title}</h2>
-    </header>
-  );
+  return <h2 className="text-2xl font-bold">{title}</h2>;
 }
 
 export function MainHeader() {
   return (
-  <Link to="/home">
-    <Header variant="main" title="Gossip with Go" />
-  </Link>
+    <Link to="/home" className="group">
+      <div className="flex flex-row">
+        <Header
+          variant="main"
+          title={
+            <>
+              Gossip with{" "}
+              <span className="text-blue-500">
+                Go
+              </span>
+            </>
+          }
+        />
+      </div>
+    </Link>
   );
 }
+
--- frontend/src/components/UserIcon.tsx
+++ frontend/src/components/UserIcon.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
 
 import Button from "./Button";
 import { useAuth } from "../Auth";
@@ -8,19 +7,34 @@ import { useAuth } from "../Auth";
 export default function UserIcon() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const { user, setUser } = useAuth();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     setUser(null);
+    navigate("/login");
+  };
+
+  const handleProfile = () => {
+    navigate(`/users/${user?.username}`);
+  }
+
+  const handleClickOutside = (event: MouseEvent) => {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.user-icon-dropdown')) {
+      setDropdownOpen(false);
+    }
   };
 
   return (
     <div>
-      <h2 onClick={() => setDropdownOpen(!dropdownOpen)}>{user?.username}</h2>
+      <h2 className="text-lg font-bold cursor-pointer border rounded-full p-2"
+      onClick={() => setDropdownOpen(!dropdownOpen)}
+      >
+        {user?.username}
+      </h2>
       {dropdownOpen && (
-        <div>
-          <Link to={`/users/${user?.username}`}>
-            <Button variant="primary" value="Profile"/>
-          </Link>
+        <div className="absolute right-5 mt-1 w-30 bg-white border rounded-lg shadow-lg flex flex-col p-2">
+            <Button variant="primary" value="Profile" onClick={handleProfile}/>
           <Button variant="square" value="Log Out" onClick={handleLogout} />
         </div>
       )}
